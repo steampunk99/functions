1800a410b8ede9c15fa8494f4395d97f2e0b38ee
steampunk99 <luk23bonnie8@gmail.com>
2023-12-20 16:48:07 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+require("cors")({ origin: true });
+
+admin.initializeApp();
+
+// Twilio credentials
+// const accountSid = 'AC4769704fa53d75b5d54fa0f6f469fbc2';
+// const authToken = '580a30caacac3efaf7c0d01bc1b63fe4';
+// const client = require('twilio')(accountSid, authToken);
+
+const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: "bonnie.lou23@outlook.com",
+      pass: "Riddickluke987",
+    }
+  });
+
+exports.sendUploadEmailNotification = functions.firestore
+  .document('uploads/{documentName}')
+  .onCreate(async (snap) => {
+
+    const newUpload = snap.data();
+
+    const mailOptions = {
+      from:"bonnie.lou23@outlook.com",
+      to: "luk23bonnie8@gmail.com",  
+      subject: "New Document Uploaded",
+      text: `A new document has been uploaded: ${newUpload.documentName}`, 
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log("Email sent successfully");
+    } catch (error) {  
+      console.error("Error sending email", error);  
+    }
+
+    
+ });
+
+ exports.sendWhatsappNotification = functions.firestore
+ .document('uploads/{documentName}')
+ .onCreate(async (snap) => {
+    const newUpload = snap.data();
+    const message = `A new document has been uploaded: ${newUpload.documentName}`;
+    await client.messages.create({
+        body: message,
+        from:'whatsapp:+14155238886',
+        to: 'whatsapp:+256782443845'
+    });
+    console.log("Whatsapp notification sent successfully");
+    return message; 
+ 
+ })
